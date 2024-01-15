144e62f6c2d3d16f89433a54d0d5f0c57ac10020
DavidDevGt <josuedavidvl18@gmail.com>
2024-01-15 14:28:25 -0600

    database for the api finished

---
+++ flask_backend/notes_api/db/database.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE notes_app;
+USE notes_app;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE notes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
