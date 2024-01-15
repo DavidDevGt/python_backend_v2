f286931654dbf9b075f5d800db043d59291d3b3b
DavidDevGt <josuedavidvl18@gmail.com>
2024-01-15 15:22:47 -0600

    create various routes for notes and users

--- flask_backend/notes_api/app/models/models.py
+++ flask_backend/notes_api/app/models/models.py
@@ -9,6 +9,14 @@ class User(db.Model):
     password = db.Column(db.String(255), nullable=False)
     notes = db.relationship('Note', backref='user', lazy=True)
     
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'email': self.email,
+            'notes': [note.to_dict() for note in self.notes]
+        }
+    
 class Note(db.Model):
     __tablename__ = 'notes'
     id = db.Column(db.Integer, primary_key=True)
@@ -16,3 +24,12 @@ class Note(db.Model):
     content = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.TIMESTAMP, server_default=db.func.current_timestamp())
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'content': self.content,
+            'created_at': str(self.created_at),
+            'user_id': self.user_id
+        }
--- flask_backend/notes_api/app/routes/views.py
+++ flask_backend/notes_api/app/routes/views.py
@@ -1,9 +1,46 @@
-from flask import Blueprint, jsonify
-from ..models.models import User, Note
+from flask import Blueprint, request, jsonify
+from ..models.models import db, User, Note
 
 views = Blueprint('views', __name__)
+
+# Views
 
 @views.route('/users', methods=['GET'])
 def get_users():
     users = User.query.all()
-    return jsonify([user.username for user in users])
+    return jsonify({'users': [user.to_dict() for user in users]})
+
+@views.route('/notes', methods=['GET'])
+def get_notes():
+    notes = Notes.query.all
+    return jsonify({'notes': [note.to_dict() for note in notes]})
+
+@views.route('/user/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    user = User.query.get_or_404(user_id)
+    return jsonify(user_to_dict())
+
+@views.route('/note/<int:note_id>', methods=['GET'])
+def get_note(note_id):
+    note = Note.query.get_or_404(note_id)
+    return jsonify(note.to_dict())
+
+# Functions for me
+def user_to_dict(user):
+    return {
+        'id': user.id,
+        'username': user.username,
+        'email': user.email
+    }
+    
+def note_to_dict(note):
+    return {
+        'id': note.id,
+        'title': note.title,
+        'content': note.content,
+        'created_at': note.created_at
+        'user_id': note.user_id
+    }
+    
+User.to_dict = user_to_dict
+Note.to_dict = note_to_dict
